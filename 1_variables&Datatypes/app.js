7cd84cb101be9e872a3ff488b64d2e7c07af44e6
RaunakKumarIITP <raunak.iitp@gmail.com>
2025-07-30 22:35:57 +0530

    initial  comit

---
+++ 1_variables&Datatypes/app.js
@@ -0,0 +1,25 @@
+// Datatypes
+
+let num= 1;
+let naam="raunak";
+let flag= true;
+let val= undefined;
+let not= null;
+
+console.log(num, naam, flag, val, not);
+console.log(typeof(num),typeof (naam), typeof(flag), typeof(val), typeof(not));
+
+
+// Variable naming- allowed and not allowed
+
+let a=1;
+let a1=2;
+let a1_=3;
+let _=4;
+let $=5;
+let A=6;
+let for=7;
+let 9a=8;
+
+
+console.log(a , a1, a1_, _, $, A, for, 9a);
