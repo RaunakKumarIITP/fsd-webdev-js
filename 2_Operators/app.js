b984fc593ca594665fd098904a05f3b74007f430
RaunakKumarIITP <raunak.iitp@gmail.com>
2025-07-30 22:59:01 +0530

    initial commit

---
+++ 2_Operators/app.js
@@ -0,0 +1,13 @@
+// Arithemetic operartor
+
+
+let a=10;
+let b= 20;
+let c=2;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%c);
+console.log(a**c);
---
+++ 3_stringBasics/app.js
@@ -0,0 +1,17 @@
+let naam= 'raunak';
+
+let course= "M.tech";
+
+let college = `iitp`;
+
+console.log(naam, course, college);
+
+
+// String concatenation
+
+console.log(naam+" "+ course +" "+ college);
+
+// string template literals
+
+console.log(`${naam} ${course} ${college} `)
+
